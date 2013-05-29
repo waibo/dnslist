8a0305e9f40075d67f116e0a33f75b08acd75494
waibo <rhythmofwolf@gmail.com>
2013-05-29 10:24:41 +0800

    update

---
+++ concentration.php
@@ -0,0 +1,7 @@
+address=/amazon.com/127.0.0.1
+address=/amazon.cn/127.0.0.1
+address=/taobao.com/127.0.0.1
+address=/twitter.com/127.0.0.1
+address=/facebook.com/127.0.0.1
+address=/plus.google.com/127.0.0.1
+address=/weibo.com/127.0.0.1
